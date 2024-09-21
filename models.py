a37cb0daa813e82d16412b51be1422019a1744af
cchalla098 <cchalla@kent.edu>
2024-09-21 14:27:39 -0400

    Initial commit for Flask Pet App

---
+++ models.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(100), nullable=False)
+
