74e3919be498187ed04b1c9b2dba8983df727174
cchalla098 <cchalla@kent.edu>
2024-09-21 12:53:47 -0400

    Initial commit for Flask Pet App

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired
-
-class PetForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    age = IntegerField('Age', validators=[DataRequired()])
-    type = StringField('Type', validators=[DataRequired()])
-    submit = SubmitField('Add Pet')
